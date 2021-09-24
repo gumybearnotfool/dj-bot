fddb4f4e21ce24b919cb43a8bf485c2cfa4dab8d
gumybearnotfool <91299934+gumybearnotfool@users.noreply.github.com>
2021-09-24 08:05:00 +0530

    Add files via upload

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: '<:Blu_No:874931698930970635>',
+        error: '<:PngItem_2010753:864759778307866636>',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: '<:Blu_Ok:874924311897604146>',
+    },
+
+    discord: {
+        token: 'ODkwNzczNjc0NjExODQzMTQ0.YU0roQ.XUNjcH8J_PPkPYoW0Qy7G5_Kd0k',
+        prefix: '>',
+        activity: 'DJ BOT IS BACK',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
